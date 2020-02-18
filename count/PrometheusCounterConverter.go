82f674a745aa05a929caf7bd891ea8de9ecb06fe
Levichev Dmitry <levichev.dmitry@gmail.com>
2020-02-18 20:16:42 +0300

    Fix TimeStamp counter converter

--- count/PrometheusCounterConverter.go
+++ count/PrometheusCounterConverter.go
@@ -65,12 +65,10 @@ func (c *TPrometheusCounterConverter) ToString(counters []*ccount.Counter, sourc
 			builder += "# TYPE " + counterName + "_count gauge\n"
 			builder += counterName + "_count" + labels + " " + cconv.StringConverter.ToString(counter.Count) + "\n"
 			break
-			// ToDo: Are needed timestamp type?
-
-			// case ccount.Timestamp: // Prometheus doesn't support non-numeric metrics
-			// 	builder += "# TYPE " + counterName + " untyped\n"
-			// 	builder += counterName + labels + " " + cconv.StringConverter.ToString(counter.Time) + "\n"
-			// 	break
+		case ccount.Timestamp: // Prometheus doesn't support non-numeric metrics
+			builder += "# TYPE " + counterName + " gauge\n" //" untyped\n"
+			builder += counterName + labels + " " + cconv.StringConverter.ToString(counter.Time.Unix()) + "\n"
+			break
 		}
 	}
 
