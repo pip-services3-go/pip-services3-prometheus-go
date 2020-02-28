c002f05d05bf1ac7041329e297cbf600f35a6ed7
Levichev Dmitry <levichev.dmitry@gmail.com>
2020-02-28 19:42:02 +0300

    Create index.go

---
+++ index.go
@@ -0,0 +1,7 @@
+package prometheus
+
+import (
+	_ "github.com/pip-services3-go/pip-services3-prometheus-go/build"
+	_ "github.com/pip-services3-go/pip-services3-prometheus-go/count"
+	_ "github.com/pip-services3-go/pip-services3-prometheus-go/services"
+)
